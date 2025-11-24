3ea566543fc29bc6ff35881e7d80219bac63075a
Lily Grace Sheram <lilygrace576@gmail.com>
2025-11-24 10:54:01 -0500

    Add files via upload
    run condor file for flasher, i dont think anything needs to be changed

---
+++ run_condor_flasher.sh
@@ -0,0 +1,34 @@
+#!/bin/bash
+# run_condor.sh
+
+source /opt/root/bin/thisroot.sh
+
+# Setup ExACT
+export EXACT_DIR=/exact
+export LD_LIBRARY_PATH=/exact/dict:/usr/lib/oracle/21/client64/lib:/usr/local/lib:$LD_LIBRARY_PATH
+
+
+DATE=$1
+FILENAME=$2
+echo "Running job for $DATE with file $FILENAME "
+echo "Directories"
+ls -lh
+
+# Create necessary directories if they don't exist
+mkdir -p DataAnalysis/MergedData/Output/$DATE/
+mkdir -p DataAnalysis/flasher_calibration/Output/
+
+mv Merged_$FILENAME DataAnalysis/MergedData/Output/$DATE/
+echo "Running FileMerge for $DATE and File $FILENAME"
+# ldd ./FileMerge
+./FlasherCalibration $DATE y
+
+cd 
+cd DataAnalysis/flasher_calibration/Output/
+chmod 774 *.root
+pwd
+ls -lh
+
+cd
+pwd
+ls -lh
