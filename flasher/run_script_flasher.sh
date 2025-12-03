4c8c94606eed27894b2bf79ecd260f6aa29a7ff3
Lilygrace <lilygrace576@gmail.com>
2025-12-03 16:23:03 -0500

    Update run_script_flasher.sh
    add folder arg

--- flasher/run_script_flasher.sh
+++ flasher/run_script_flasher.sh
@@ -3,7 +3,10 @@
 
 DATE=$1
 FILE=$2
+# add folder arg
+FOLDER=$3
 echo "Running for $DATE"
-apptainer exec --bind /storage/osg-otte1/shared/TrinityDemonstrator:/mnt /storage/osg-otte1/shared/TrinityDemonstrator/DataAnalysis/containers/rootandexact.sif /mnt/DataAnalysis/flasher_calibration/FlasherCalibration $DATE y $FILE
+# add folder arg input
+apptainer exec --bind /storage/osg-otte1/shared/TrinityDemonstrator:/mnt /storage/osg-otte1/shared/TrinityDemonstrator/DataAnalysis/containers/rootandexact.sif /mnt/DataAnalysis/flasher_calibration/FlasherCalibration $DATE y $FILE $FOLDER
 
 # sleep 10
