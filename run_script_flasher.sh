e3ed2f650678d336e76480b9ddeef8542e9e0399
Lilygrace <lilygrace576@gmail.com>
2025-12-03 12:46:32 -0500

    Delete run_script_flasher.sh

--- run_script_flasher.sh
+++
@@ -1,9 +0,0 @@
-#!/bin/bash
-# run_script.sh
-
-DATE=$1
-FILE=$2
-echo "Running for $DATE"
-apptainer exec --bind /storage/osg-otte1/shared/TrinityDemonstrator:/mnt /storage/osg-otte1/shared/TrinityDemonstrator/DataAnalysis/containers/rootandexact.sif /mnt/DataAnalysis/flasher_calibration/FlasherCalibration $DATE y $FILE
-
-# sleep 10
