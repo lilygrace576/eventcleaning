8e3e671a8afeacb46372376084a87dbf3c3bda9f
Lilygrace <lilygrace576@gmail.com>
2025-12-03 15:55:02 -0500

    Update run_script_evcl.sh
    include folder arg

--- event_cleaning/run_script_evcl.sh
+++ event_cleaning/run_script_evcl.sh
@@ -3,5 +3,8 @@
 
 DATE=$1
 FILENAME=$2
+# add folder arg
+FOLDER=$3
 echo "Running for $DATE"
-apptainer exec --bind /storage/osg-otte1/shared/TrinityDemonstrator:/mnt /storage/osg-otte1/shared/TrinityDemonstrator/DataAnalysis/containers/rootandexact.sif /mnt/DataAnalysis/event_cleaning/ClusterCleaning/EventCleaning $DATE y $FILENAME    
+# add folder arg
+apptainer exec --bind /storage/osg-otte1/shared/TrinityDemonstrator:/mnt /storage/osg-otte1/shared/TrinityDemonstrator/DataAnalysis/containers/rootandexact.sif /mnt/DataAnalysis/event_cleaning/ClusterCleaning/EventCleaning $DATE y $FILENAME $FOLDER    
